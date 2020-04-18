a7b833dd622a5abf00b4c2d916ff14b26e03a11b
pongpong41 <pong7658@gmail.com>
2020-04-19 01:32:49 +0700

    auth guard

--- memdrill-frontend/src/app/app-routing.module.ts
+++ memdrill-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { GameComponent } from './game/game.component';
 import { RegisterComponent } from './user/register/register.component';
 import { StatComponent } from './home/stat/stat.component';
 
 const routes: Routes = [
-  { path: '', component: StatComponent },
+  { path: '', component: StatComponent, canActivate: [AuthGuard]},
   { path: 'auth/login', component: SignInComponent },
   { path: 'user/register', component: RegisterComponent },
   { path: 'game', component: GameComponent }
