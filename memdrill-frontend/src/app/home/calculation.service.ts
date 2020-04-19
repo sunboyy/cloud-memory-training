ab71dff1d9786f49669050c02e837cfb7945aea7
sunboyy <sura_sun@hotmail.com>
2020-04-19 14:39:05 +0700

    Refactor stats

--- memdrill-frontend/src/app/home/calculation.service.ts
+++ memdrill-frontend/src/app/home/calculation.service.ts
@@ -1,26 +1,23 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
-import { APIResponse } from '../interfaces';
 import { Stat } from './stat/stat';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculationService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   private getHeaders(): { [header: string]: string | string[] } {
-    return { Authorization: 'Bearer ' + sessionStorage.getItem('accessToken') };
+    return { Authorization: 'Bearer ' + this.authService.getAccessToken() };
   }
 
-  stats(): Observable<Stat> {
-    return this.http
-      .get<Stat>(environment.baseApiUrl + 'calculation/stats', {
-        headers: this.getHeaders()
-      });
+  getStats(): Observable<Stat> {
+    return this.http.get<Stat>(environment.baseApiUrl + 'calculation/stats', {
+      headers: this.getHeaders()
+    });
   }
 }
