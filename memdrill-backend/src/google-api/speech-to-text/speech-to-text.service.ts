50a815ae866cbdc322af74ee4eb53c26e289dd92
sunboyy <sura_sun@hotmail.com>
2020-04-19 23:36:37 +0700

    Refactor speech to text service 1

--- memdrill-backend/src/google-api/speech-to-text/speech-to-text.service.ts
+++ memdrill-backend/src/google-api/speech-to-text/speech-to-text.service.ts
@@ -1,52 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import speech from '@google-cloud/speech';
-import * as linear16 from 'linear16';
-import * as fs from 'fs';
+import { SpeechClient } from '@google-cloud/speech';
+import linear16 from 'linear16';
+import { readFileSync, writeFileSync } from 'fs';
 
 @Injectable()
 export class SpeechToTextService {
   async toText(base64data: string): Promise<string> {
     const tempOriginal = 'temp.weba';
     const tempRaw = 'tempraw.raw';
-    
+
     const buff = new Buffer(base64data, 'base64');
-    fs.writeFileSync(tempOriginal, buff);
-    var audioBytes;
+    writeFileSync(tempOriginal, buff);
 
     // Returns the output path, ex: ./output.wav
     const tempRawPath = await linear16(tempOriginal, tempRaw);
-      
-    const file = fs.readFileSync(tempRawPath);
-    audioBytes = file.toString('base64');
-    
-    // const audioBytes = base64data;
-    const client = new speech.SpeechClient();
-    const audio = {
-      content: audioBytes,
-    };
-    const config = {
-      encoding: 'LINEAR16',
-      sampleRateHertz: 16000,
-      languageCode: 'th-TH',
-    };
-    const request = {
-      audio: audio,
-      config: config,
-    };
+
+    const file = readFileSync(tempRawPath);
+    const audioBytes = file.toString('base64');
     console.log('audioBytes:' + audioBytes);
-    // // Detects speech in the audio file
+
+    return await this.transcribeLinear16(audioBytes);
+  }
+
+  async transcribeLinear16(base64AudioBytes: string): Promise<string> {
+    const client = new SpeechClient();
     const [response] = await client.recognize({
-      audio: audio,
+      audio: {
+        content: base64AudioBytes,
+      },
       config: {
         encoding: 'LINEAR16',
         sampleRateHertz: 16000,
-        languageCode: 'th-TH'
-      }
+        languageCode: 'th-TH',
+      },
     });
     const transcription = response.results
       .map(result => result.alternatives[0].transcript)
       .join('\n');
-    console.log(`Transcription: ${transcription}`);
     return transcription;
   }
 }
